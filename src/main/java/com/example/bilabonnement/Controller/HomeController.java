f02f7c606e31ccd41c8d4cc92acc53f7e80feccd
Math33t3 <math33t3@stud.kea.dk>
2022-05-06 13:53:38 +0200

    startside

--- src/main/java/com/example/bilabonnement/Controller/HomeController.java
+++ src/main/java/com/example/bilabonnement/Controller/HomeController.java
@@ -1,9 +1,24 @@
 package com.example.bilabonnement.Controller;
 
+import com.example.bilabonnement.Service.BilService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+@Autowired
+BilService bilService;
+
+    @GetMapping("/")
+    public String startside(){
+        return "/startside";
+    }
+
+    @GetMapping("/opretBil")
+    public String opretBil(){
+        return "/opretBil";
+    }
 
 
 }
