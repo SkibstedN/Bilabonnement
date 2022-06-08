478153345ed3186c39867211e28937e415062deb
Inukbot <mart1b85@stud.kea.dk>
2022-06-08 15:19:48 +0200

    Stylesheet i alle html

---
+++ src/test/java/com/example/bilabonnement/Controller/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.bilabonnement.Controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @Test
+    void startsideDataregistrering() {
+
+    }
+}
